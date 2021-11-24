ae890bbfe79dafc2fdec80bacc03aea36e62c57a
engin.karatas@dvu.com.tr <43602725+EnginKARATAS@users.noreply.github.com>
2021-11-24 21:09:16 +0300

    tweetted via python

---
+++ mention/hello.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import tweepy
+#from our keys module (keys.py), import the keys dictionary
+from keys import keys
+
+CONSUMER_KEY = keys['consumer_key']
+CONSUMER_SECRET = keys['consumer_secret']
+ACCESS_TOKEN = keys['access_token']
+ACCESS_TOKEN_SECRET = keys['access_token_secret']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+twts = api.search(q="Hello World!")
+
+#list of specific strings we want to check for in Tweets
+t = ['Hello world!',
+    'Hello World!',
+    'Hello World!!!',
+    'Hello world!!!',
+    'Hello, world!',
+    'Hello, World!']
+
+for s in twt:
+    for i in t:
+        if i == s.text:
+            sn = s.user.screen_name
+            m = "@%s Hello!" % (sn)
+            s = api.update_status(m, s.id)
---
+++ twitter.py
@@ -0,0 +1,13 @@
+import tweepy
+from config import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+ 
+# print(api.mentions_timeline())
+# api.update_status("This Tweet was Tweeted using Tweepy!")
+
+
