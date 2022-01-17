e4ea85c6b3e763fdbf30d7e3dace2bebce1369e8
engin.karatas@dvu.com.tr <43602725+EnginKARATAS@users.noreply.github.com>
2022-01-18 00:31:46 +0300

    python project upated

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-consumer_key = "OgQAffhhNj8yuJPiXD4qqW3c4" 
 consumer_secret = "63aTmsMipYKbS77rsKZLERgPCjI2Jxzup6LFBeDCwJn9hWvH03" 
+consumer_key = "OgQAffhhNj8yuJPiXD4qqW3c4" 
 access_token = "1374444599461113862-gIR01S0nsAZoItZVSoXk5cLFUeKrTq"  
 access_token_secret = "QrjxLWkz7K1YhJpq3LJZhMaYh5PIQ46d0626kbfzXtjcO" 
--- test.py
+++ test.py
@@ -4,28 +4,19 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from textblob import TextBlob
 from elasticsearch import Elasticsearch
-
-
 from config import *
-
 
 es = Elasticsearch()
 
-
 class TweetStreamListener(StreamListener):
-
 
     def on_data(self, data):
 
-
         dict_data = json.loads(data)
-
 
         tweet = TextBlob(dict_data["text"])
 
-
         print (tweet.sentiment.polarity)
-
 
         if tweet.sentiment.polarity < 0:
             sentiment = "olumsuz"
@@ -34,9 +25,7 @@ class TweetStreamListener(StreamListener):
         else:
             sentiment = "olumlu"
 
-
         print (sentiment)
- 
 
         es.index(index="twitter2",
                  doc_type="test-type",
@@ -53,21 +42,16 @@ class TweetStreamListener(StreamListener):
                        "sentiment": sentiment})
         return True
 
-
     def on_error(self, status):
         print (status)
 
 if __name__ == '__main__':
 
-
     listener = TweetStreamListener()
-
 
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
-
     stream = Stream(auth, listener)
-
 
     stream.filter(track=['beatles']) 
